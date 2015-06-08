da4c32968a0be3afa2d23faba1fd76d2b42afb12
zhuwr <wrzhu@buaa.edu.cn>
2015-06-08 15:36:13 +0800

    Create Minimum Subarray1.java

---
+++ Minimum Subarray1.java
@@ -0,0 +1,27 @@
+
+// 2661ms
+public class Solution {
+    /**
+     * @param nums: a list of integers
+     * @return: A integer indicate the sum of minimum subarray
+     */
+    public int minSubArray(ArrayList<Integer> nums) {
+        // write your code
+        if (nums == null) {
+            return 0;
+        }
+        
+        int min = nums.get(0);
+        int previousSum = min;
+        int size = nums.size();
+    
+        for (int i = 1; i < size; i++) {
+            int current = nums.get(i);
+            previousSum = Math.min(current, current + previousSum);
+            min = Math.min(min, previousSum);//min > previousSum? previousSum : min;
+        }
+        
+        return min;
+    }
+}
+
