3dac721441075d48361b95c55f7f3e65943e0d36
zhuwr <wrzhu@buaa.edu.cn>
2015-06-09 15:40:44 +0800

    Create Implement Queue by Two Stacks1.java

---
+++ Implement Queue by Two Stacks1.java
@@ -0,0 +1,40 @@
+// 1702ms
+public class Solution {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public Solution() {
+       // do initialization if necessary
+        stack1 = new Stack<Integer>();
+        stack2 = new Stack<Integer>();
+    }
+    
+    public void push(int element) {
+        // write your code here
+        stack1.push(element);
+    }
+
+    public int pop() {
+        // write your code here
+        if(stack2.isEmpty()) {
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.peek());
+                stack1.pop();
+            }
+        }
+        return stack2.pop();
+         
+    }
+
+    public int top() {
+        // write your code here
+        if(stack2.isEmpty()){
+        while(!stack1.isEmpty()) {
+            stack2.push(stack1.peek());
+            stack1.pop();
+        }}
+        return stack2.peek();
+
+    }
+}
+
