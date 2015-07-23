118523ff7460d727fa2295fc2705f0cf9ad3dff4
zhuwr <wrzhu@buaa.edu.cn>
2015-07-23 09:02:05 +0800

    寻找旋转排序数组中的最小值

---
+++ C++/Medium/Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /**
+     * @param num: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    int findMin(vector<int> &num) {
+        // write your code here
+        for(int i=0; i<num.size()-1; i++) {
+            if(num[i]>num[i+1]) {
+                return num[i+1];
+            }
+        }
+        return num[0];
+    }
+};
